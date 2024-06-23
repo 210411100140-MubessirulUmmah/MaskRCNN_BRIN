3cde76a87b90263135311427c3c2ef70b459ba17
210411100140-MubessirulUmmah <mubarijojo.ummah11@gmail.com>
2024-06-23 17:00:32 +0700

    'e'

--- Mask_R-CNN_BRIN.py
+++ Mask_R-CNN_BRIN.py
@@ -2,7 +2,9 @@ import streamlit as st
 import cv2
 import numpy as np
 import torch
+import detectron2
 from detectron2.engine import DefaultPredictor
+
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
@@ -15,7 +17,7 @@ def load_trained_model():
     cfg.MODEL.DEVICE = "cpu"
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-    cfg.MODEL.WEIGHTS = "E:/Mask R-CNN BRIN/detectron2/detectron2/model_zoo/configs/COCO-InstanceSegmentation/model_final (1).pth"  # Path ke model yang telah dilatih
+    cfg.MODEL.WEIGHTS = "E:/Mask_R-CNN/detectron2/detectron2/model_zoo/configs/COCO-InstanceSegmentation/model_final (1).pth"  # Path ke model yang telah dilatih
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # Sesuaikan dengan jumlah kelas dalam dataset kamu
     
     # Register dataset dan metadata
