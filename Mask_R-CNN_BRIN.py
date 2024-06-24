fdbf631e6ec857892d2d939f147b5d47a4b278ff
210411100140-MubessirulUmmah <mubarijojo.ummah11@gmail.com>
2024-06-24 11:02:25 +0700

    'editlagi33'

--- Mask_R-CNN_BRIN.py
+++ Mask_R-CNN_BRIN.py
@@ -14,10 +14,10 @@ sys.path.insert(0, detectron2_path)
 
 # from detectron2.engine import DefaultPredictor
 
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2 import model_zoo
+# from detectron2.config import get_cfg
+# from detectron2.utils.visualizer import Visualizer
+# from detectron2.data import MetadataCatalog, DatasetCatalog
+# from detectron2 import model_zoo
 
 # Fungsi untuk memuat model yang telah dilatih
 @st.cache_resource
